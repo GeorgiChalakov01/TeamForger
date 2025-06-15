0aff63997a8fecb7de2d0b909b36d22fa1d96e43
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-06-15 18:42:17 +0300

    Created a separate directory and package for every one of the sections.

--- backend/app/pages/signup/signup_templ.go
+++ backend/app/pages/signup/signup_templ.go
@@ -11,7 +11,7 @@ import templruntime "github.com/a-h/templ/runtime"
 import (
 	"teamforger/backend/core"
 	"teamforger/backend/pages/layout"
-	"teamforger/backend/pages/signup/sections"
+	"teamforger/backend/pages/signup/sections/signupForm"
 )
 
 func SignUp() templ.Component {
@@ -35,7 +35,7 @@ func SignUp() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = layout.Base(false, core.User{}, sections.SignUpForm()).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = layout.Base(false, core.User{}, signupForm.SignUpForm()).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
